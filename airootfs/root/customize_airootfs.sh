82b0dea15d2176aa9a79aaca2b5ab8dc57ccd453
Jamie Quigley <jamie@jamiequigley.com>
2020-03-01 00:12:06 +0000

    Added dotfiles (part 1)

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -9,6 +9,19 @@ ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
 usermod -s /usr/bin/zsh root
 cp -aT /etc/skel/ /root/
+
+# Install dotfiles
+rm -f /root/.zshrc
+ln -s /.dotfiles/zshrc /root/.zshrc
+ln -s /.dotfiles/aliases.zsh /root/.zsh/
+ln -s /.dotfiles/functions.zsh /root/.zsh/
+ln -s /.dotfiles/zsh-plugins /root/.zsh/plugins
+
+mkdir -p /root/.config/nvim
+ln -s /.dotfiles/init.vim /root/.config/nvim/init.vim
+
+nvim +PlugInstall
+
 chmod 700 /root
 
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
